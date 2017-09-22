190c42b2d48409047da57ae18e23eb232a72ed47
miketheo423 <miketheo423@gmail.com>
2017-09-22 00:21:31 -0600

    Adds keystroke movement of bikes

--- js/app.js
+++ js/app.js
@@ -1,13 +1,34 @@
+$(function(){
+	console.log('Up and Running');
+
+
 //Define the two images as variables
-var blueBike = $('#bluebike');
-var orangeBike = $('#orangeBike');
-
 //Select the images using JQuery
-
+var blueBike = $('#blueBike');
+var orangeBike = $('#orangeBike');
+var blueMove = 0;
+var orangeMove = 0;
+var blueValue = $('#blueBike').css('padding-left');
 //Set event listeners on the images for keystrokes
-
 //When keystroke is pushed, padding-left will be placed on the image to move it accross the screen
 	//Use a percentage increase to help with scaling
-	//Increase the padding by 5% or 10%
+	//Increase the padding by 5% 
+$('body').keyup(function (e) {
+	if (e.keyCode == 65) {
+		blueMove += 5;
+		$('#blueBike').css("padding-left", blueMove + "%");
+		console.log(blueValue);
+	}
+});
 
+$('body').keyup(function (e) {
+	if (e.keyCode == 76) {
+		orangeMove += 5;
+		$('#orangeBike').css("padding-left", orangeMove + "%");
+	}
+});
 //When the image reaches a certain point, the game will display an alert that the player has won and the game will resest 
+if($('#blueBike').css('padding-left')=== "100%"){
+	console.log("We have a winner");
+}
+});
