bc06c7ca56f09c54a0d6498f18c45afcebfea662
miketheo423 <miketheo423@gmail.com>
2017-09-21 22:33:27 -0600

    Adds game page

---
+++ js/app.js
@@ -0,0 +1,13 @@
+//Define the two images as variables
+var blueBike = $('#bluebike');
+var orangeBike = $('#orangeBike');
+
+//Select the images using JQuery
+
+//Set event listeners on the images for keystrokes
+
+//When keystroke is pushed, padding-left will be placed on the image to move it accross the screen
+	//Use a percentage increase to help with scaling
+	//Increase the padding by 5% or 10%
+
+//When the image reaches a certain point, the game will display an alert that the player has won and the game will resest 
