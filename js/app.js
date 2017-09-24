42c37b5458995d3ccd2968c323eea3448ba28426
miketheo423 <miketheo423@gmail.com>
2017-09-23 21:59:55 -0600

    Fixes comments

--- js/app.js
+++ js/app.js
@@ -1,18 +1,20 @@
 $(function(){
-//Define the two images as variables
-//Select the images using JQuery
+	
 var blueBike = $('#blueBike');
 var orangeBike = $('#orangeBike');
 var blueMove = 0;
 var orangeMove = 0;
 var playerOneWin = 0;
 var playerTwoWin = 0;
-//Set event listeners on the images for keystrokes
-//When keystroke is pushed, padding-left will be placed on the image to move it accross the screen
-	//Use a percentage increase to help with scaling
-	//Increase the padding by 5% 
+/**
+*Set event listeners on the images for keystrokes
+*When keystroke is pushed, padding-left will be placed on the image to move it accross the screen
+*Use a percentage increase to help with scaling
+*Increase the padding by 5% 
+**/
 
 //Moves the blue bike (Player One)	
+
 $('body').keyup(function (e) {
 	if (e.keyCode == 65) {
 		blueMove += 5;
